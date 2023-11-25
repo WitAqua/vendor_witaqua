92ed8d9dc326ff580ea7343d353b56443322711f
Pranav Vashi <neobuddy89@gmail.com>
2023-11-25 09:21:23 +0530

    common: Inherit pixel-fw when available
    Change-Id: Ibfa8222e1ca90a4004da51befdf043fefb182311
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/witaqua.mk)
+$(call inherit-product-if-exists, vendor/pixel-framework/config.mk)
 
 PRODUCT_BRAND ?= WitAqua
 
