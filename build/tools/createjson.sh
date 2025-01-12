c503161430837d91dbc0f4971db547766b9a8f53
Lup Gabriel <gwolf2u.website@gmail.com>
2025-01-12 11:10:58 +0200

    witaqua: rework `createjson.sh` (#65)
    Change-Id: Icd9ddcecbfb274eb7aaf68c6709d39e7ff17a9f5

--- build/tools/createjson.sh
+++ build/tools/createjson.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Copyright (C) 2019-2023 crDroid Android Project
+# Copyright (C) 2019-2025 crDroid Android Project
 #			(C) 2025 WitAqua
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# You may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -20,78 +20,61 @@
 existingOTAjson=./WitAquaOTA/data/$1.json
 output=$2/$1.json
 
-#cleanup old file
+# Cleanup old file
 if [ -f $output ]; then
-	rm $output
+    rm $output
 fi
 
 echo "Generating JSON file data for OTA support..."
 
+# Helper function to extract field from JSON
+extract_field() {
+    grep -Po "\"$1\"\s*:\s*\"[^\"]*\"" "$existingOTAjson" | head -n 1 | sed -E 's/.*: \"(.*)\"/\1/'
+}
+
 if [ -f $existingOTAjson ]; then
-	#get data from already existing device json
-	#there might be a better way to parse json yet here we try without adding more dependencies like jq
-	maintainer=`grep -n "\"maintainer\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-	oem=`grep -n "\"oem\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-	device=`grep -n "\"device\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-	filename=$3
-	version=`echo "$3" | cut -d'-' -f5`
-	v_max=`echo "$version" | cut -d'.' -f1 | cut -d'v' -f2`
-	v_min=`echo "$version" | cut -d'.' -f2`
-	version=`echo $v_max.$v_min`
-	buildprop=$2/system/build.prop
-	linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
-	timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
-	md5=`md5sum "$2/$3" | cut -d' ' -f1`
-	sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
-	size=`stat -c "%s" "$2/$3"`
-	echo '{
-	"response": [
-		{
-			"maintainer": "'$maintainer'",
-			"oem": "'$oem'",
-			"device": "'$device'",
-			"filename": "'$filename'",
-			"download": "https://sourceforge.net/projects/witaqua/files/'$version'/'$1'/'$3'/download",
-			"timestamp": '$timestamp',
-			"md5": "'$md5'",
-			"sha256": "'$sha256'",
-			"size": '$size',
-			"version": "'$version'"
-		}
-	]
-}' >> $output
-else
-	filename=$3
-	version=`echo "$3" | cut -d'-' -f5`
-	v_max=`echo "$version" | cut -d'.' -f1 | cut -d'v' -f2`
-	v_min=`echo "$version" | cut -d'.' -f2`
-	version=`echo $v_max.$v_min`
-	buildprop=$2/system/build.prop
-	linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
-	timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
-	md5=`md5sum "$2/$3" | cut -d' ' -f1`
-	sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
-	size=`stat -c "%s" "$2/$3"`
-
-	echo '{
-	"response": [
-		{
-			"maintainer": "''",
-			"oem": "''",
-			"device": "''",
-			"filename": "'$filename'",
-			"download": "https://sourceforge.net/projects/crdroid/files/'$1'/'$v_max'.x/'$3'/download",
-			"timestamp": '$timestamp',
-			"md5": "'$md5'",
-			"sha256": "'$sha256'",
-			"size": '$size',
-			"version": "'$version'"
-		}
-	]
-}' >> $output
-
-	echo 'There is no official support for this device yet'
-	echo 'Consider adding official support by reading the documentation at https://wiki.witaqua.org/developers/maintainership/requirements.html'
+    # Extract fields from existing JSON or leave empty
+    MAINTAINER=$(extract_field "maintainer")
+    OEM=$(extract_field "oem")
+    DEVICE=$(extract_field "device")
 fi
 
-echo ""
+# Generate JSON fields
+FILENAME=$3
+VERSION=$(echo "$3" | cut -d'-' -f5 | sed 's/v//')
+V_MAX=$(echo "$VERSION" | cut -d'.' -f1)
+V_MIN=$(echo "$VERSION" | cut -d'.' -f2)
+VERSION="$V_MAX.$V_MIN"
+
+BUILDPROP="$2/system/build.prop"
+TIMESTAMP=$(grep "ro.system.build.date.utc" "$BUILDPROP" | cut -d'=' -f2)
+MD5=$(md5sum "$2/$3" | cut -d' ' -f1)
+SHA256=$(sha256sum "$2/$3" | cut -d' ' -f1)
+SIZE=$(stat -c "%s" "$2/$3")
+
+# Generate JSON output
+cat <<EOF >$output
+{
+    "response": [
+        {
+            "maintainer": "${MAINTAINER:-}",
+            "oem": "${OEM:-}",
+            "device": "${DEVICE:-}",
+            "filename": "$FILENAME",
+            "download": "https://sourceforge.net/projects/witaqua/files/$VERSION/$1/$3/download",
+            "timestamp": $TIMESTAMP,
+            "md5": "$MD5",
+            "sha256": "$SHA256",
+            "size": $SIZE,
+            "version": "$VERSION"
+        }
+    ]
+}
+EOF
+
+if [ ! -f $existingOTAjson ]; then
+    echo "There is no official support for this device yet"
+    echo "Consider adding official support by reading the documentation at https://wiki.witaqua.org/developers/maintainership/requirements.html"
+fi
+
+echo "JSON file generation completed"
