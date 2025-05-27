61466d690da78aa38b666794a89feb59a38a2cec
toufu <hamm157famm7423@gmail.com>
2025-05-27 14:26:46 +0900

    witaqua: Include OmniJaws
    Change-Id: I8454dde1aa0e37f078177804f5f77777c3b495fe

--- config/witaqua.mk
+++ config/witaqua.mk
@@ -64,3 +64,6 @@ PRODUCT_COPY_FILES += \
 # Cloned app exemption
  PRODUCT_COPY_FILES += \
      vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-witaqua-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-witaqua-product.xml
+
+PRODUCT_PACKAGES += \
+    OmniJaws
