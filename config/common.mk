1a13f79ab5017b3cc0281ec8f6c8ff941288e85d
Toufu <hamm157famm7423@gmail.com>
2025-10-26 16:49:50 +0900

    witaqua: remove SetupWizardOverlay

--- config/common.mk
+++ config/common.mk
@@ -273,8 +273,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     DocumentsUIOverlay \
     NetworkStackOverlay \
-    PermissionControllerOverlay \
-    SetupWizardOverlay
+    PermissionControllerOverlay
 
 # Allow to disable camera sounds optionally
 PRODUCT_PACKAGES += \
