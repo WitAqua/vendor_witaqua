b40a28d435b81e5b9d292221cd8b8c7201ef4379
Toufu <hamm157famm7423@gmail.com>
2026-05-25 09:10:59 +0000

    witaqua: Add our config
    Change-Id: I363200870ccfff237835b188a6bca3e239fdd35a

---
+++ config/common.mk
@@ -0,0 +1,7 @@
+PRODUCT_BRAND ?= WitAqua
+
+# Version
+include vendor/witaqua/config/version.mk
+
+# Keys
+-include vendor/witaqua-priv/keys/keys.mk
---
+++ config/version.mk
@@ -0,0 +1,20 @@
+PRODUCT_VERSION_MAJOR := 3
+PRODUCT_VERSION_MINOR := 2
+
+WITAQUA_BUILD_TYPE ?= UNOFFICIAL
+WITAQUA_MAINTAINER ?= Unknown
+
+# Versioning System
+WITAQUA_BUILD_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
+
+# Internal version
+LINEAGE_VERSION := WitAqua-v$(WITAQUA_BUILD_VERSION)-$(shell date -u +%Y%m%d)-$(LINEAGE_BUILD)-$(WITAQUA_BUILD_TYPE)
+
+# Display version
+LINEAGE_DISPLAY_VERSION := $(LINEAGE_VERSION)
+
+# WitAqua version properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.witaqua.build.version=$(WITAQUA_BUILD_VERSION) \
+    ro.witaqua.build.status=$(WITAQUA_BUILD_TYPE) \
+    ro.witaqua.maintainer=$(WITAQUA_MAINTAINER)
