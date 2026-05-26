dd5dadfafd8d59ce13847cbe35a58777122bc55e
Toufu <hamm157famm7423@gmail.com>
2026-05-26 12:55:21 +0000

    witaqua: Build our overlay
    Change-Id: If19b5d6a47f78264e74ae9f571beabaf071b86f6

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -1,3 +1,10 @@
 # WitAqua-ify
 PRODUCT_PACKAGES += \
 	WitAquaFrameworksResOverlay
+
+# Standard Overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/witaqua/overlay/common
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/witaqua/overlay/common
