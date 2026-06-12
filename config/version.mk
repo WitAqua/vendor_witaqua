61a056eb36930a75f30437429a264ce1823bb0ec
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-13 01:13:26 +0900

    version: Add custom camera info support
    Change-Id: If10e07cdfbd97ecd82b727b68ec3f5ffb3c222c8

--- config/version.mk
+++ config/version.mk
@@ -23,4 +23,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PROCESSOR_INFO ?= Unknown
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.sys.processor_info=$(subst $() ,_,$(PROCESSOR_INFO))
+    persist.sys.processor_info=$(subst $() ,_,$(PROCESSOR_INFO)) \
+    persist.sys.device_camera_info_rear=$(CAMERA_REAR_INFO) \
+    persist.sys.device_camera_info_front=$(CAMERA_FRONT_INFO)
