e5ff2ed70830434ae62404541a1a28a93deb76cb
kdrag0n <danny@kdrag0n.dev>
2026-06-13 00:38:37 +0900

    audio: add AOSP Material sounds
    The default AOSP ringtone, alarm, and notification sounds are quite
    lackluster in comparison to the proprietary Pixel sounds. Replace them
    with better ones that are licensed permissively:
      - AOSP Material sounds from Android 9
        frameworks/base/data/sounds/AudioPackage14.mk
      - Material Product Sounds [1], licensed under CC-BY 4.0
        https://material.io/design/sound/sound-resources.html
    
    Change-Id: Ie678085034d71a0e26bb7e0955999c1b97a5082b

--- config/audio.mk
+++ config/audio.mk
@@ -8,7 +8,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ringtones,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones)
 
+# 2019 Material product sounds (CC-BY 4.0)
+# Source: https://material.io/design/sound/sound-resources.html
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/material/,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
+
 # Default Sounds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Frail_Naysayer.ogg \
-    ro.config.alarm_alert=Birdland.m4a
+    ro.config.alarm_alert=Birdland.m4a \
+    ro.config.notification_sound=material_simple-celebration_1.ogg
