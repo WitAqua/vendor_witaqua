ecbef50267ecfd79cdcd3e1a979f8fb58916d892
AndroPlus-org <hosigumayuugi@gmail.com>
2026-06-13 00:59:55 +0900

    overlay: Conditionally disable camera sounds
    * Original module: https://github.com/AndroPlus-org/magisk-module-camerasound
    
    Change-Id: Idafb4b624ad5b911fdc15ca4b192bf79b95da31f

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -2,6 +2,10 @@
 PRODUCT_PACKAGES += \
 	WitAquaFrameworksResOverlay
 
+# Optional
+PRODUCT_PACKAGES += \
+    DisableCameraSoundOverlay
+
 # Standard Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
 	vendor/witaqua/overlay/common
