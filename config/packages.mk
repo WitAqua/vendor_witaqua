9dfa3e3ada772a1adbad0ca50ce75c2bcaf0d10e
kailua <kailua@project2by2.jp>
2026-06-13 00:52:52 +0900

    packages: Build FaceUnlock
    Change-Id: Icf1fa18f3e69919e8d85c309ac24ee69a6225bae

--- config/packages.mk
+++ config/packages.mk
@@ -9,3 +9,15 @@ ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
   PRODUCT_PACKAGES += su
   PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += system/xbin/su
 endif
+
+# Face Unlock
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
