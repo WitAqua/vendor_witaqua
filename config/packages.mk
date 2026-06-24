feb34ffd3c2b72fca8a6ece65417b8ab1e6f468a
Toufu <hamm157famm7423@gmail.com>
2026-06-24 04:12:44 +0000

    witaqua: config: Build BtHelper
    Change-Id: Id9d3c26058d3ae625915774600e19fbf6477ba8c

--- config/packages.mk
+++ config/packages.mk
@@ -27,3 +27,6 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
+
+PRODUCT_PACKAGES += \
+    BtHelper
